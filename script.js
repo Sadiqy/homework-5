1913810f6cab96c475a4acc85ee0b00e36a4e525
Git Sadiq <abdulaisadiq@live.com>
2020-02-03 21:38:45 -0800

    Added event listener for save buttons + utilized classes to indicate past/present/future timeslots

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ currentHour.toLocaleString()
 
 let leadEl = $("#currentDay");
 leadEl.text(currentTime);
+
+
+document.addEventListener("click", function(event){
+    event.preventDefault();
+    if(event.target.type === 'submit'){
+         let index = event.target.getAttribute('data-time-index');
+    let inputfield = document.querySelector(`#content-${index}`).value;
+    console.log(index, inputfield);
+    localStorage.setItem(index, inputfield);
+    }
+})
+
+for (let i = 0; i < 9; i++){
+    let textArea = document.querySelector(`#content-${i}`);
+    let todo = localStorage.getItem(i) || ''; 
+    textArea.textContent = todo;
+    if(i+9<currentHour){
+        textArea.classList.add('past')
+    }else if(i+9=== currentHour){
+        textArea.classList.add('present')
+    }else{
+        textArea.classList.add('future')
+    }
+}
+     
+
+
